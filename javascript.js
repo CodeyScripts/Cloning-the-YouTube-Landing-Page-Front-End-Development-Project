c1eeb450a0a5594a90dc2f513d4886f267fc5b84
Munir Danjuma <munirdanjuma@gmail.com>
2024-06-09 12:06:36 +0100

    initial commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var MenuIcon = document.querySelector(".toggleButton");
+var SideBar = document.querySelector(".sidebar");
+
+MenuIcon.onclick = function(){
+    side.classlist.toggle("small-sidebar")
+}
